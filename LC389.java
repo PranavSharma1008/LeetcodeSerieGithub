4b9e3d1286a2e6d3f181c700db5c1a60262b27bb
PranavSharma <pranav2410991479@gmail.com>
2026-02-21 22:11:48 +0530

    LC389 Push

---
+++ LC389.java
@@ -0,0 +1,28 @@
+
+
+// Find The Difference
+
+
+
+
+// class Solution {
+//     public char findTheDifference(String s, String t) {
+//         int n1= s.length();
+//         int n2 = t.length();
+
+//         if(n1==0) return t.charAt(0);
+//         char [] arr1 = s.toCharArray(s);
+//         char [] arr2 = t.toCharArray(t);
+
+//         Arrays.sort(s);
+//         Arrays.sort(t);
+
+//         for(int i=0; i<n1; i++){
+//             if(s.charAt(i)!=t.charAt(i)){
+//                 return t.charAt(i));
+//             }
+//         }
+//         return t.charAt(n-1));
+        
+//     }
+// }
